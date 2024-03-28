f38b5f8043f3d524b9cc5872a03d88f773367e85
Benoît Pasquiet <benoit.pasquiet@ifce.fr>
2024-03-28 14:19:45 +0100

    Ajout d'un exemple pour Yolo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Vision par ordinateur : Quel regard sur nos chevaux ?
 
-Ce répertoire met à disposition les sources des exemples présentés aux Journées Sciences et Innovation Equines 2024.
+Ce répertoire met à disposition plusieurs exemples présentés aux Journées Sciences et Innovation Equines 2024.
 
+
+Citation :
 Pasquiet, Benoît. « Vision par ordinateur : Quel regard sur nos chevaux ? » In Journées Sciences et Innovations Equines. Saumur, 2024.
--- utils/download.py
+++ utils/download.py
@@ -1,12 +1,17 @@
 '''Utilitaires pour télécharger des images'''
+import ssl
 import urllib.request
 from pathlib import Path
+
+import certifi
 
 def download_file(url:str, output_name=None, output_dir="downloads", force=False):
     '''Téléchargement d'un fichier vers un dossier download, si celui-ci n'existe pas encore'''
     if output_name is None:
         output_name = Path(url).name
-    response = urllib.request.urlopen(url)
+    response = urllib.request.urlopen(
+        url,
+        context=ssl.create_default_context(cafile=certifi.where()))
     if output_name is None:
         output_name = response.headers.get_filename()
     output_file = Path(output_dir) / output_name
